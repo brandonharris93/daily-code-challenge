310673a939e270aab09a18d27247c63a4de3afe0
brandonharris93 <brandonharris93@gmail.com>
2020-05-08 11:46:13 -0500

    first challenge

---
+++ 050420.py
@@ -0,0 +1,14 @@
+def recursive_stairs(step_sizes,num_steps,cache):
+    if num_steps in cache:
+        return cache[num_steps]
+    elif num_steps<0:
+        return 0
+    else:
+        combinations = 0
+        for s in step_sizes:
+            combinations += recursive_stairs(step_sizes,num_steps-s,cache)
+        return combinations
+
+def stairs(step_sizes,num_steps):
+    cache = {0: 1}
+    return recursive_stairs(step_sizes,num_steps,cache)    
