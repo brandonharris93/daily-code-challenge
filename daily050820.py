f0bd49ecc3eeb4d4179977550b0338b691d70741
brandonharris93 <brandonharris93@gmail.com>
2020-05-09 13:45:35 -0500

    corrected yesterday and added new solution

--- daily050820.py
+++ daily050820.py
@@ -11,9 +11,30 @@ You can modify the input array in-place.
 import numpy as np
 
 def lowest_pos_int(input):
+    #this solution actually does not fit the parameters given, as it is supposed to use constant space and linear time
+    #this line uses linear space
     indices = np.ones(len(input)+1,dtype=bool)
     for i in input:
         if i > 0 and i <= len(input)+1:
             indices[i-1] = False
 
     return np.argmax(indices) + 1
+
+def lowest_pos_int_alt(input):
+    #this alternative solution should use less space since it modifies the input list in place rather than creating
+    #a parallel boolean array
+    min = 1
+    max = len(input)
+    for i in range(max):
+        tmp = input[i]
+        if min<=tmp and tmp<=max:
+            #swap each number within the possible range for its place in sequence
+            input[tmp-1], input[i] = tmp, input[tmp-1]
+    #if a number is in the array, it has been put in its appropriate place in the sequence
+    #therefore if the number in the array doesn't match its (1-based) index number, it is missing
+    #the first such number we find is the lowest missing positive integer
+    for i in range(max):
+        if i+1 != input[i]:
+            return i+1
+    return max+1
+
